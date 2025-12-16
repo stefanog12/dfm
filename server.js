07b3aa38e62fede50842e1b23c36a35f393630dc
stefanog12 <79454706+stefanog12@users.noreply.github.com>
2025-12-17 00:30:33 +0100

    Update server.js
    token unlimited

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ fastify.register(async (fastify) => {
                     instructions: BASE_SYSTEM_MESSAGE,
                     modalities: ["text", "audio"],
                     temperature: 0.8,
-					max_response_output_tokens: 150,
+					// max_response_output_tokens: 150,
                     input_audio_transcription: {
                         model: "whisper-1"
                     }
