8e818b80985b0b0702f49b7da7555dd5a0106fec
stefanog12 <79454706+stefanog12@users.noreply.github.com>
2025-12-22 23:20:30 +0100

    Update server.js
    fix logs

--- server.js
+++ server.js
@@ -69,9 +69,9 @@ fastify.register(async (fastify) => {
                 session: {
                     turn_detection: { 
                         type: 'server_vad',
-                        threshold: 0.55,
+                        threshold: 0.75,
                         prefix_padding_ms: 200,
-                        silence_duration_ms: 500
+                        silence_duration_ms: 300
                     },
                     input_audio_format: 'g711_ulaw',    // IMPORTANT: Twilio sends PCMU
                     output_audio_format: 'g711_ulaw',   // Match PCMU output
@@ -143,28 +143,7 @@ fastify.register(async (fastify) => {
 				
 // Reinvia session.update dopo session.created
 				if (msg.type === "session.created") {
-						setTimeout(() => {
-						console.log("+2 secondi : Re-sending session.update after session.created");
-							openAiWs.send(JSON.stringify({
-								type: "session.update",
-								session: {
-										turn_detection: { 
-											type: "server_vad",
-											threshold: 0.75,
-											prefix_padding_ms: 200,
-											silence_duration_ms: 500
-										},
-										input_audio_format: "g711_ulaw",
-										output_audio_format: "g711_ulaw",
-										voice: "alloy",
-										instructions: BASE_SYSTEM_MESSAGE,
-										modalities: ["text", "audio"],
-										temperature: 0.8,
-										// ?? QUESTA È LA CHIAVE 
-										include: ["session"]
-								}
-							}));
-						}, 10000); // 10 secondi
+					console.log("? SESSION CREATED RECEIVED");	
 				}
 
 				if (msg.type === "session.updated") {
